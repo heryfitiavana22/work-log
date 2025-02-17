b46266ff91320424a2934a04448b1ae9400be256
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-17 20:28:16 +0300

    Add AccessLog and AccessType classes for logging access events

---
+++ quarkus/src/main/java/org/worklog/accesslog/AccessLog.java
@@ -0,0 +1,108 @@
+package org.worklog.accesslog;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AccessLog {
+    private Long id;
+    private String cardId;
+    private String deviceId;
+    private String employeeId;
+    private String employeeName;
+    private LocalDate date;
+    private LocalTime time;
+    private AccessType type;
+
+    public AccessLog() {
+    }
+
+    public AccessLog(Long id, String cardId, String deviceId, String employeeId, String employeeName, LocalDate date,
+            LocalTime time, AccessType type) {
+        this.id = id;
+        this.cardId = cardId;
+        this.deviceId = deviceId;
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.date = date;
+        this.time = time;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public AccessType getType() {
+        return type;
+    }
+
+    public void setType(AccessType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "AccessLog{" +
+                "id=" + id +
+                ", cardId='" + cardId + '\'' +
+                ", deviceId='" + deviceId + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                ", employeeName='" + employeeName + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ quarkus/src/main/java/org/worklog/accesslog/AccessType.java
@@ -0,0 +1,6 @@
+package org.worklog.accesslog;
+
+public enum AccessType {
+    entry,
+    exit
+}
