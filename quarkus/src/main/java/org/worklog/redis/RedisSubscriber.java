f5fe063aab5b68631e198842121d308edcf413ce
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-18 21:13:51 +0300

    Implement RedisSubscriber class for handling FilebeatData notifications via Redis pub/sub

--- quarkus/src/main/java/org/worklog/redis/RedisSubscriber.java
+++ quarkus/src/main/java/org/worklog/redis/RedisSubscriber.java
@@ -1,5 +1,38 @@
 package org.worklog.redis;
 
-public class RedisSubscriber {
-    
+import java.util.function.Consumer;
+
+import org.worklog.filebeat.FilebeatData;
+
+import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.pubsub.PubSubCommands;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import jakarta.annotation.PreDestroy;
+
+public class RedisSubscriber implements Consumer<FilebeatData> {
+    private final PubSubCommands<FilebeatData> pub;
+    private final PubSubCommands.RedisSubscriber subscriber;
+
+    public RedisSubscriber(RedisDataSource ds) {
+        pub = ds.pubsub(FilebeatData.class);
+        subscriber = pub.subscribe("logs-channel", this);
+    }
+
+    @Override
+    public void accept(FilebeatData notification) {
+        Uni.createFrom().item(notification)
+                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
+                .subscribe().with(notif -> {
+                    System.out.println(notif.getJsonFromCsv());
+                    // TODO: send to elasticsearch
+                }, failure -> {
+                    failure.printStackTrace();
+                });
+    }
+
+    @PreDestroy
+    public void terminate() {
+        subscriber.unsubscribe();
+    }
 }
