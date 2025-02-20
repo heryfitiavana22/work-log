84d7aa8f60ba1b0860b6db17337faedaf708cde2
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-20 20:50:21 +0300

    Add AccessLogService class with Hibernate Search mapping integration

---
+++ quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
@@ -0,0 +1,12 @@
+package org.worklog.accesslog;
+
+import org.hibernate.search.mapper.pojo.standalone.mapping.SearchMapping;
+
+import jakarta.inject.Inject;
+
+public class AccessLogService {
+    @Inject
+    SearchMapping searchMapping;
+
+    
+}
