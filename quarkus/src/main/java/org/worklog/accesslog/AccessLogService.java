9a78cd025745da3d7e8dcd56dc2aed16e07b3c5e
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-20 20:54:07 +0300

    Add save method to AccessLogService for indexing access logs

--- quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
+++ quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
@@ -8,5 +8,13 @@ public class AccessLogService {
     @Inject
     SearchMapping searchMapping;
 
-    
+    public AccessLog save(AccessLog accessLog) {
+        try (var searchSession = searchMapping.createSession()) {
+            searchSession.indexingPlan().add(accessLog);
+            return accessLog;
+        } catch (Exception e) {
+            // TODO: throw custom error
+            throw new RuntimeException();
+        }
+    }
 }
