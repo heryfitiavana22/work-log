32de4cc51d0d42bdfbdfaf8db4cad51299ac3e3e
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-21 21:38:24 +0300

    Add ElascticsearchSession class for managing Hibernate Search sessions

---
+++ quarkus/src/main/java/org/worklog/elasticsearch/ElascticsearchSession.java
@@ -0,0 +1,23 @@
+package org.worklog.elasticsearch;
+
+import org.hibernate.search.mapper.pojo.standalone.mapping.SearchMapping;
+import org.hibernate.search.mapper.pojo.standalone.session.SearchSession;
+
+import jakarta.inject.Inject;
+
+public class ElascticsearchSession {
+    @Inject
+    SearchMapping searchMapping;
+
+    public interface SessionCallback<T> {
+        T doInSession(SearchSession searchSession) throws Exception;
+    }
+
+    public <T> T withSession(SessionCallback<T> callback) throws CreateSessionSearchException {
+        try (var searchSession = searchMapping.createSession()) {
+            return callback.doInSession(searchSession);
+        } catch (Exception e) {
+            throw new CreateSessionSearchException();
+        }
+    }
+}
