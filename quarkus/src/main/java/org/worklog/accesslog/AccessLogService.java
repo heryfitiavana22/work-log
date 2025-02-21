5e76232b64cb346a49684e1d2b669b85305e0ec7
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-02-21 21:44:09 +0300

    Refactor AccessLogService to extend ElascticsearchSession for improved session management

--- quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
+++ quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
@@ -1,20 +1,14 @@
 package org.worklog.accesslog;
 
-import org.hibernate.search.mapper.pojo.standalone.mapping.SearchMapping;
 import org.worklog.elasticsearch.CreateSessionSearchException;
+import org.worklog.elasticsearch.ElascticsearchSession;
 
-import jakarta.inject.Inject;
-
-public class AccessLogService {
-    @Inject
-    SearchMapping searchMapping;
+public class AccessLogService extends ElascticsearchSession {
 
     public AccessLog save(AccessLog accessLog) throws CreateSessionSearchException {
-        try (var searchSession = searchMapping.createSession()) {
+        return withSession(searchSession -> {
             searchSession.indexingPlan().add(accessLog);
             return accessLog;
-        } catch (Exception e) {
-            throw new CreateSessionSearchException();
-        }
+        });
     }
 }
