6b4cece473fa2116bb22e4c841dec8d920ebed90
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-02 15:41:02 +0300

    Add ApplicationScoped annotation to AccessLogService for improved dependency injection

--- quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
+++ quarkus/src/main/java/org/worklog/accesslog/AccessLogService.java
@@ -3,6 +3,9 @@ package org.worklog.accesslog;
 import org.worklog.elasticsearch.ElascticsearchSession;
 import org.worklog.elasticsearch.exception.CreateSessionSearchException;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class AccessLogService extends ElascticsearchSession {
 
     public AccessLog save(AccessLog accessLog) throws CreateSessionSearchException {
