35cce181c4d8f2f54814d974512c239309840519
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-08 14:11:23 +0300

    Implement CSV appending functionality in DataCsvService

--- quarkus/src/main/java/org/worklog/csv/DataCsvService.java
+++ quarkus/src/main/java/org/worklog/csv/DataCsvService.java
@@ -1,5 +1,8 @@
 package org.worklog.csv;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import org.worklog.accesslog.AccessLog;
@@ -14,7 +17,19 @@ public class DataCsvService {
         this.csvFilePath = Paths.get(System.getProperty("user.dir"), "..", "db.csv").toString();
     }
 
-    public void appendToCSV(AccessLog transactionfFlat)  {
-        // TODO: append data to csv file
+    public void appendToCSV(AccessLog accessLog) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath, true))) {
+            String csvLine = String.format("%d,%s,%s,%s,%s,%s,%s\n",
+                    accessLog.getId(),
+                    accessLog.getCardId(),
+                    accessLog.getDeviceId(),
+                    accessLog.getEmployeeId(),
+                    accessLog.getEmployeeName(),
+                    accessLog.getTimestamp(),
+                    accessLog.getType());
+            writer.write(csvLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
